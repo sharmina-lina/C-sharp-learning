01621d95fd3fe7f8bf8c1dac962486964996cd29
sharmina-lina <92200149+sharmina-lina@users.noreply.github.com>
2023-02-23 13:00:13 +0100

    Create Pupil.cs

---
+++ Pupil.cs
@@ -0,0 +1,19 @@
+// Pupil.cs
+using System;
+
+namespace MagicalInheritance
+{
+  class Pupil
+  {
+    public string Title
+    {get; private set;}
+    public Pupil(string title){
+        Title = title;
+    }
+    public Storm CastWindStrom(){
+      Storm Pupil = new Storm("Wind",false, Title);
+      return Pupil;
+
+    }
+  }
+}
